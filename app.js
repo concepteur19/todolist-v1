6953796305837dc46118d6056cbd2c4fbd7ea5c2
concepteur19 <zobel.tchomgui@gmail.com>
2022-06-09 10:41:05 +0100

    modifying 139

--- app.js
+++ app.js
@@ -136,8 +136,12 @@ app.post('/delete', function(req, res) {
     
 });
 
+let port = process.env.PORT;
+if (port == null || port == "" ) {
+    port = 3000;
+};
 
-app.listen(process.env.PORT || 4000, function() {
+app.listen( port, function() {
     console.log("server start on port 4000");
 }); 
 
